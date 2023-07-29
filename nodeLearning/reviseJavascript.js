9dd548447adb695ed1e249a9504bb480bc2128bd
ravikeshri2910 <ravikeshri2910@gmail.com>
2023-07-29 12:40:00 +0530

    node-javascript-revision

---
+++ nodeLearning/reviseJavascript.js
@@ -0,0 +1,21 @@
+
+const multiply = (a,b) => {
+    return a*b
+}
+
+console.log(multiply(5,2))
+
+
+const student = {
+
+    name : "Abay",
+    roll_no : 1,
+    age : 25,
+    passed(){
+        return "Passed all subject"
+    }
+
+}
+
+console.log(student.age)
+console.log(student.passed())
