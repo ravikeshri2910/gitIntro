98785cc2166ae54c24f33aaed04c38208c5d7fbd
ravikeshri2910 <ravikeshri2910@gmail.com>
2023-07-30 16:07:12 +0530

    Request/Response

--- nodeLearning/app.js
+++ nodeLearning/app.js
@@ -1,10 +1,49 @@
 const http = require("http");
 
 
-const server = http.createServer((req,res)=>{
-
-    console.log(req)
+const server = http.createServer((req, res) => {
+    // console.log(req)
     console.log("Ravi prasad keshri")
+    console.log(req.url)
+
+    if (req.url == '/home') {
+
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Home</title></head>');
+        res.write('<body><h2>Welcome Home</h2></body>');
+        res.write('</html>');
+        res.end();
+        process.exit()
+    }
+    else if (req.url == '/about') {
+
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Home</title></head>');
+        res.write('<body><h2>Welcome To About Page</h2></body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if (req.url == '/node') {
+
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Home</title></head>');
+        res.write('<body><h2>Welcome To my Node Js Project</h2></body>');
+        res.write('</html>');
+        res.end();
+    }
+    else{
+
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Home</title></head>');
+        res.write('<body><h2>Invalid URL</h2></body>');
+        res.write('</html>');
+        res.end();
+    }
+    // process.exit()
 })
 
 server.listen(4000);
