be15680edbeac380595dd3ea0a51807fbf7f5737
ravikeshri2910 <ravikeshri2910@gmail.com>
2023-08-01 15:13:02 +0530

    Writing and reading from a file

--- nodeLearning/app.js
+++ nodeLearning/app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 
 const server = http.createServer((req, res) => {
@@ -14,13 +15,13 @@ const server = http.createServer((req, res) => {
         res.write('<body><h2>Welcome Home</h2></body>');
         res.write('</html>');
         res.end();
-        process.exit()
+        // process.exit()
     }
     else if (req.url == '/about') {
 
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
-        res.write('<head><title>Home</title></head>');
+        res.write('<head><title>About</title></head>');
         res.write('<body><h2>Welcome To About Page</h2></body>');
         res.write('</html>');
         res.end();
@@ -29,16 +30,57 @@ const server = http.createServer((req, res) => {
 
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
-        res.write('<head><title>Home</title></head>');
+        res.write('<head><title>Node</title></head>');
         res.write('<body><h2>Welcome To my Node Js Project</h2></body>');
         res.write('</html>');
         res.end();
     }
-    else{
+    else if (req.url === '/') {
+        // let file = "Ram"
+
+        fs.readFile('newText.txt', { encoding: "utf-8" }, (err, data) => {
+            if(err){
+                console.log(err)
+            }
+
+            res.setHeader('Content-Type', 'text/html');
+            res.write('<html>');
+            res.write('<head><title>Node</title></head>');
+            res.write(`<body><h2>${data}</h2></body>`);
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>');
+
+            res.write('</html>');
+            res.end();
+        })
+    }
+
+    else if (req.url === '/message' && req.method === 'POST') {
+        console.log(req.method)
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        })
+        return req.on("end", () => {
+            const parseBody = Buffer.concat(body).toString()
+            console.log(parseBody);
+            const msg = parseBody.split("=")[1]
+
+            // fs.writeFileSync('newText.txt', msg);
+            fs.writeFile('newText.txt', msg, () => {
+
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        })
+
+    }
+    else {
 
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
-        res.write('<head><title>Home</title></head>');
+        res.write('<head><title>None</title></head>');
         res.write('<body><h2>Invalid URL</h2></body>');
         res.write('</html>');
         res.end();
