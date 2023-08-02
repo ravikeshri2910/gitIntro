34509c981efc403286f7587433716e5586b9aae3
ravikeshri2910 <ravikeshri2910@gmail.com>
2023-08-02 23:48:16 +0530

    Simple Group Chat Application

---
+++ nodeLearning/chatApp.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+{/* <form onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/product" method="POST">
+
+	<input id="username" type="text" name"title">
+
+	<button type="submit">add</button> */}
+
+// </form>
+
+//<form onSubmit="localStorage.setItem("username", (document.getElementById('username').value))" action="/logedin" method="POST">
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded())
+
+
+app.get("/login", (req, res, next) => {
+    res.status(202).send(`
+    <body>
+    <form onSubmit="localStorage.setItem('username', document.getElementById('username').value)" action="/logedin" method="POST">
+    <label>User:-</label>
+    <input id="username" type="text" name="title">
+    <input id="text" type="hidden" name="text">
+    <br>
+    <button type="submit">Log In</button>
+    </form>
+    </body>`);
+
+})
+
+function add(){
+    let user = document.getElementById('username').value;
+    localStorage.setItem('username',user)
+}
+
+
+
+
+// document.getElementById("username").value
+
+app.post("/logedin", (req, res) => {
+    
+    console.log(req.body)
+    fs.writeFile("chatText.txt",`${req.body.title}:${req.body.text}` ,{flag : 'a'}, ()=>{
+        res.redirect("/")
+
+    })
+})
+
+app.get("/", (req, res) => {
+
+    fs.readFile("chatText.txt",(err, data)=>{
+        if(err){
+            data = "No chat found"
+        }
+        res.status(202).send(`
+        <body>
+        <h2>${data}</h2>
+        <form action="/logedin" method="POST">
+        <input id="username" type="hidden" name="title">
+        <label>Chat:-</label>
+        <input id="text" type="text" name="text">
+        <br>
+        <button type="submit">Enter</button>
+        </form>
+        </body>`);
+    })
+    
+    // let text = req.body.title;
+   
+
+
+
+
+})
+
+
+
+app.listen(3000);
+
+
+
